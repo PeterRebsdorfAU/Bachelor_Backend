65f388be35c166d824d41612bec098c3c456ebca
PeterRebsdorf <202206699@post.au.dk>
2025-10-01 12:54:53 +0200

    Fixed some endpoints and some logic with dates

--- obj/Debug/net8.0/Bachelor_Backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Bachelor_Backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bachelor_Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e67ccddb27e46e6fc1f4ca6171097d26bfcc7f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cca8a7cfa8ceab4e6a67b5779c81c6e61033c82")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bachelor_Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bachelor_Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
